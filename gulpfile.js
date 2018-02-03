47aca9fa66c49c71d90ad365e02c5bcfc5910f4b
Romain Liautaud <romain.liautaud@ens-lyon.fr>
2018-02-03 15:48:33 +0100

    Switched to tozti-gulp for building.

--- gulpfile.js
+++ gulpfile.js
@@ -1,52 +1,10 @@
-var gulp = require('gulp');
-var uglify = require('gulp-uglify-es').default;
-var sass = require('gulp-sass');
-var imagemin = require('gulp-imagemin');
-var del = require('del');
+'use strict'
 
-var paths = {
-    scripts: ['assets/js/**'],
-    images: 'assets/img/**',
-    styles: 'assets/sass/**',
-    fonts: 'assets/fonts/**'
-};
+let gulp = require('tozti-gulp')
 
-gulp.task('clean', function() {
-    return del(['dist']);
-});
-
-gulp.task('scripts', ['clean'], function() {
-    return gulp.src('assets/js/index.js')
-          .pipe(uglify())
-          .pipe(gulp.dest('dist/js'));
-});
-
-gulp.task('fonts', ['clean'], function () {
-    return gulp.src(paths.fonts)
-          .pipe(gulp.dest('dist/fonts'));
-});
-
-gulp.task('images', ['clean'], function() {
-    return gulp.src(paths.images)
-               .pipe(imagemin({
-                    interlaced: true,
-                    progressive: true,
-                    optimizationLevel: 5,
-                }))
-               .pipe(gulp.dest('dist/img'));
-});
-
-gulp.task('sass', function () {
-    return gulp.src(paths.styles)
-               .pipe(sass().on('error', sass.logError))
-               .pipe(gulp.dest('dist/css'));
-});
-
-gulp.task('watch', function() {
-    gulp.watch(paths.scripts, ['scripts']);
-    gulp.watch(paths.images, ['images']);
-    gulp.watch(paths.fonts, ['fonts']);
-    gulp.watch(paths.styles, ['sass']);
-});
-
-gulp.task('default', ['watch', 'scripts', 'images', 'sass', 'fonts']);
+gulp({
+    scripts: ['src/index.js'],
+    fonts: ['assets/fonts'],
+    images: ['assets/img'], 
+    styles: ['assets/sass/style.scss'],
+})
